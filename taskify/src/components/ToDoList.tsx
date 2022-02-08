094a242b0f1aaad0365bdfc40b26ea85cf2ca89b
Marian-Janolek <m.janolek@gmail.com>
2022-02-08 19:18:18 +0100

    add dragable todo list

---
+++ taskify/src/components/ToDoList.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Droppable } from 'react-beautiful-dnd';
+import { Todo } from '../model';
+import SingleTodo from './SingleTodo';
+import './styles.css';
+
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  completedTodos: Todo[];
+  setCompletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const ToDoList: React.FC<Props> = ({
+  todos,
+  setTodos,
+  completedTodos,
+  setCompletedTodos,
+}) => {
+  return (
+    <div className="container">
+      {' '}
+      <Droppable droppableId="TodosList">
+        {(provided, snapshot) => (
+          <div
+            className={`todos ${snapshot.isDraggingOver ? 'dragactive' : ''}`}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className="todos-heading">active tasks</span>
+            {todos.map((todo, index) => (
+              <SingleTodo
+                index={index}
+                todo={todo}
+                todos={todos}
+                key={todo.id}
+                setTodos={setTodos}
+              />
+            ))}{' '}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+      <Droppable droppableId="TodosRemove">
+        {(provided, snapshot) => (
+          <div
+            className={`todos  ${
+              snapshot.isDraggingOver ? 'dragcomplete' : 'remove'
+            }`}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className="todos-heading">completed tasks</span>
+            {completedTodos.map((todo, index) => (
+              <SingleTodo
+                index={index}
+                todo={todo}
+                todos={completedTodos}
+                key={todo.id}
+                setTodos={setCompletedTodos}
+              />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </div>
+  );
+};
+
+export default ToDoList;
